b599acef7a5204a6c08162357af358fbe447399a
Michel Maia <dimermichel@gmail.com>
2020-11-10 22:05:49 -0500

    Add Forgot Password Page

---
+++ src/pages/ForgotPassword/index.tsx
@@ -0,0 +1,88 @@
+import React, { useRef, useCallback, useState } from 'react';
+import { FiLogIn, FiMail } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
+
+import { useToast } from '../../hooks/Toast';
+import getValidationErrors from '../../utils/getValidationErrors';
+import logoImg from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, AnimationContainer, Background } from './styles';
+import api from '../../services/api';
+
+interface ForgotPasswordFormData {
+  email: string;
+}
+
+const ForgotPassword: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+
+  const handleSubmit = useCallback(
+    async (data: ForgotPasswordFormData) => {
+      try {
+        setLoading(true);
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('Email is required')
+            .email('Valid email is required'),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+
+        await api.post('/password/forgot', { email: data.email });
+
+        addToast({
+          type: 'success',
+          title: 'Successfully sent reset password email',
+          description: 'An email was sent to reset your password',
+        });
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+          formRef.current?.setErrors(errors);
+        } else {
+          addToast({
+            type: 'error',
+            title: 'Forgot Password Error',
+            description:
+              'An Error occurred while attempting to send the forgot password email',
+          });
+        }
+      } finally {
+        setLoading(false);
+      }
+    },
+    [addToast],
+  );
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Reset Password</h1>
+            <Input name="email" icon={FiMail} placeholder="Email" />
+
+            <Button loading={loading} type="submit">
+              Continue
+            </Button>
+          </Form>
+          <Link to="/">
+            <FiLogIn />
+            Back to Login
+          </Link>
+        </AnimationContainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default ForgotPassword;
